81236571ef31dc2ce79ade9f488ed80e37a64a0f
Ly Sochetra <lysochetra95@gmail.com>
2024-06-13 06:25:56 +0700

    UpdateV6

--- src/app/creator/page.tsx
+++ src/app/creator/page.tsx
@@ -1,5 +1,10 @@
+import Image from "next/image"
+import potatoimage from './../media/Home-removebg-preview (1).png'
+
 export default function Creator(){
     return(<> 
-    <h1>Hello this is creator</h1>
+    <div className="creator-img-con" >
+        <Image id="creator-img" src={potatoimage} alt="This is an Creator of this page" ></Image>
+    </div>
     </>)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import Footer from "./component/footer/page";
 import './globals.css'
 import ReactConfetti from 'react-confetti';
 import { useEffect, useState } from "react";
@@ -55,20 +55,10 @@ export default function Home() {
     if(count % 2 === 0){
       setDisplayCL("hiddenblock")
       setDisplayH("")
-      setSpinicon("spiniconback")
-        setTimeout(() => {
-            setSpinicon("")
-            setBarhometext("")
-        }, 1000);
     }
     else{
       setDisplayH("hiddenblock")
       setDisplayCL("")
-      setSpinicon("spinicon")
-        setTimeout(() => {
-            setSpinicon("freezespin")
-            setBarhometext("hidden")
-        }, 1000);
     }
   }
   const handlesubmit = () => {
@@ -111,32 +101,6 @@ export default function Home() {
     }
     //Muteaudio()
   }
-
-  {/* FOOTER SECTION */}
-  let [Spinicon, setSpinicon] = useState<string>()
-    let [Barhometext, setBarhometext] = useState<string>("")
-    const router = useRouter()
-    const editorpage = () => console.log("under maintanence")
-    //useEffect(() => {}, [])
-    const handlespin = () => {
-        count ++
-        console.log("it trigger in footer componenent")
-       if(count % 2 === 0){
-        setSpinicon("spiniconback")
-        setTimeout(() => {
-            setSpinicon("")
-            setBarhometext("")
-        }, 1000);
-       }
-       else{
-        setSpinicon("spinicon")
-        setTimeout(() => {
-            setSpinicon("freezespin")
-            setBarhometext("hidden")
-        }, 1000);
-       }
-
-    }
 
 
   return (<>
@@ -203,23 +167,7 @@ export default function Home() {
     </div>
   </div>
   <div>
-  <footer className="footer-main-con">
-        <div className="footer-sub-con" >
-            <div className="footer-home-con" >                
-                <Link className={`footer-home ${Barhometext}`} href='/'>Home</Link>
-            </div>
-            <div className="plus-icon-con" >
-                <div className={`plus-icon ${Spinicon}`}>
-                    <svg onClick={() => {handleicon()}} id="plus-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
-                    fill="white"/></svg>
-                </div>
-            </div>
-            <div className="footer-creator-con">
-                <Link className="footer-creator" href='/creator'>Creator</Link>
-            </div>
-            {/*<div><h2 className="footer-creator" onClick={callback} >Creator</h2></div>*/}
-        </div>
-    </footer>
+  <Footer callbackicon={handleicon}></Footer>
   </div>
   
   </>
